6568fb093b6452ffbeb424da8c34dcc87212f1e5
Larskerstens <larskerstens10@gmail.com>
2021-05-04 19:21:51 +0200

    add movie and counter

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,36 @@
 import Head from "next/head";
-import { Heading, Flex } from "@chakra-ui/react";
+import {
+  Heading,
+  Flex,
+  ScaleFade,
+  useDisclosure,
+  Button,
+  Box,
+} from "@chakra-ui/react";
 
 export default function index({ person }) {
+  const { isOpen, onToggle } = useDisclosure();
+
   return (
     <>
       <Head>
         <title>Cocktail pagina</title>
       </Head>
+
       <Flex justifyContent="center" alignItems="center" flexDirection="column">
         <Heading padding=".5em">Oefening op nextjs</Heading>
-        <Heading fontSize="1.5em">Peroonlijke info</Heading>
-        <p>Naam: {person.name}</p>
-        <p>Leeftijd: {person.age}</p>
-        <p>Postcode: {person.postcode}</p>
-        <p>Gemeente: {person.gemeente}</p>
+        <Heading fontSize="1.5em" mb="1.5em">
+          Peroonlijke info
+        </Heading>
+        <Button onClick={onToggle}>Klik hier voor mijn info</Button>
+        <ScaleFade initialScale={0.9} in={isOpen}>
+          <Box p="40px" color="white" mt="4" rounded="md" shadow="md">
+            <p>Naam: {person.name}</p>
+            <p>Leeftijd: {person.age}</p>
+            <p>Postcode: {person.postcode}</p>
+            <p>Gemeente: {person.gemeente}</p>
+          </Box>
+        </ScaleFade>
       </Flex>
     </>
   );
